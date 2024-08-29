b9fc3ea998b8a54d9e5e7b71e4642743368c1094
Victor Tabacoff <victortabacoff2007@gmail.com>
2024-08-29 08:26:20 -0500

    Initial Commit

---
+++ ImageChallenge/ImageChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageChallengeApp.swift
+//  ImageChallenge
+//
+//  Created by Victor B. Tabacoff on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
